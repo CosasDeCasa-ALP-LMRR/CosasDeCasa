db234354177a673ecf1d62f888e48734dfcd3f76
agustn134 <agustinparralopez1@gmail.com>
2026-06-12 15:48:22 -0600

    feat: agrega titulo de la vista principal

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Vista Principal - Cosas de Casa</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
